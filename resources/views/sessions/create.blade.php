dd8a4964a719af6dfb0454eaaee97cacac31844d
youki <zyx7153841@126.com>
2019-07-15 14:56:38 +0000

    登录时记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -24,6 +24,13 @@
           <input type="password" class="form-control" name="password" id="password" value="{{ old('password') }}">
         </div>
 
+        <div class="form-group">
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">记住我</label>
+          </div>
+        </div>
+
         <button type="submit" class="btn btn-primary">登录</button>
 
       </form>
