98db37d9196ca7d77f62af9611aabcdfeea44945
youki <zyx7153841@126.com>
2019-07-16 06:58:08 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'youki';
         $user->email = 'youki@123.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
