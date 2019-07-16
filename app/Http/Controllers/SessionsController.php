031414e1883ad90e7fa60a7486d34f7ddfe48458
youki <zyx7153841@126.com>
2019-07-16 08:10:16 +0000

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,9 +28,18 @@ class SessionsController extends Controller
 
         if(Auth::attempt($credentials, $request->has('remember'))) {
             //登录成功后的操作
-            session()->flash('success', '欢迎回来！');
-            $fallback = route('users.show', Auth::user());
-            return redirect()->intended($fallback);
+            if(Auth::user()->activated) {
+                //邮箱验证成功后的操作
+                session()->flash('success', '欢迎回来！');
+                $fallback = route('users.show', Auth::user());
+                return redirect()->intended($fallback);
+            }
+            else {
+                //邮箱未激活时的操作
+                Auth::logout;
+                session()->flash('warning', '您的账号未激活，请检查邮箱中的注册邮件进行激活');
+                return redirect('/');
+            }
         } else {
             //登录失败后的操作
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
